1f1420a5e87a1a8e82681ffa8abc72da9f433a1c
Diego Martin <martind@vitessepsp.com>
2024-08-07 15:02:56 +0200

    Added NSwag environment condition

--- src/WebApiOne/Program.cs
+++ src/WebApiOne/Program.cs
@@ -4,6 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApiDocument();
 builder.Services.AddEndpointsApiExplorer();
 
+// The goal is to skip certain things when running NSwag
+// View the nswag.json to see how NSwag is set as environment
+var isNSwagExecution = builder.Environment.IsEnvironment("NSwag");
+if (!isNSwagExecution)
+{
+    throw new Exception("Sample exception to demonstrate that NSwag should skip this");
+}
+
 var app = builder.Build();
 
 // Use NSwag
